f3313b39806b0b742ec76f3ae5107ae70a640c5c
harshilarora2005 <harshil.arora205@gmail.com>
2026-07-16 13:23:07 +0530

    Add RabbitMQ config with durable job.ready queue

---
+++ scheduler-service/src/main/java/com/jobr/scheduler_service/config/RabbitConfig.java
@@ -0,0 +1,16 @@
+package com.jobr.scheduler_service.config;
+
+import org.springframework.amqp.core.Queue;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitConfig {
+
+    public static final String JOB_READY_QUEUE = "job.ready";
+
+    @Bean
+    public Queue jobReadyQueue() {
+        return new Queue(JOB_READY_QUEUE, true); 
+    }
+}
